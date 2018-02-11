eb874e5937f9411b19185c32e08b5a1296494498
mvcbotelho <marcus.itec@gmail.com>
2018-02-11 00:18:57 -0300

    Adicionando mensagem de erro em casos de erro

--- public/js/frase.js
+++ public/js/frase.js
@@ -1,13 +1,21 @@
 $("#botao-frase").click(fraseAleatoria);
 
 function fraseAleatoria() {
-    $.get('http://localhost:3000/frases', function (data) {
-        var frase = $('.frase');
-        var numeroAleatorio = Math.floor(Math.random() * data.length);
+    $.get('http://localhost:3000/frases', trocaFraseAleatoria)
+        .fail(function () {
+            $('#erro').toggle();
 
-        frase.text(data[numeroAleatorio].texto);
-        atualizaTamanhoFrase();
-        atualizaTempoInicial(data[numeroAleatorio].tempo);
+            setTimeout(function (){
+                $('#erro').toggle();
+            }, 2000);
+        });
+}
 
-    });
+function trocaFraseAleatoria(data) {
+    var frase = $('.frase');
+    var numeroAleatorio = Math.floor(Math.random() * data.length);
+
+    frase.text(data[numeroAleatorio].texto);
+    atualizaTamanhoFrase();
+    atualizaTempoInicial(data[numeroAleatorio].tempo);
 }
