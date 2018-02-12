f9ec79e4c5cf13c2948a0464ce95f7d4cec806bb
mvcbotelho <marcus.itec@gmail.com>
2018-02-11 21:21:52 -0300

    Implementação do placar

--- public/js/placar.js
+++ public/js/placar.js
@@ -1,4 +1,5 @@
 $('#botao-placar').click(mostraPlacar);
+$('#botao-sync').click(sincronizaPlacar);
 
 function inserePlacar() {
     var corpoTabela = $(".placar").find("tbody");
@@ -55,4 +56,39 @@ function removeLinha() {
 
 function mostraPlacar() {
     $('.placar').stop().slideToggle(600);
+}
+
+function sincronizaPlacar() {
+    var placar = [];
+    var linhas = $('tbody > tr');
+    
+    linhas.each(function () {
+        var usuario = $(this).find('td:nth-child(1)').text();
+        var palavras = $(this).find('td:nth-child(2)').text();
+
+        var score = {
+            usuarios: usuario,
+            pontos: palavras
+        };
+
+        placar.push(score);
+    });
+
+    var dados = {
+        placar: placar
+    };
+
+    $.post('http://localhost:3000/placar', dados, function () {
+        console.log('Salvando dados no servidor');
+    });
+}
+
+function atualizaPlacar() {
+    $.get('http://localhost:3000/placar', function (data) {
+        $(data).each(function () {
+            var linha = novaLinha(this.usuarios, this.pontos);
+            linha.find('.botao-remover').click(removeLinha);
+            $('tbody').append(linha);
+        });
+    });
 }
